5fc7333b4dc849495779e5a0e6cd29145fcf50dd
haphamo <hapham0392@gmail.com>
2020-04-20 19:42:26 -0400

    Added dev server

---
+++ server/data-files/resumeData.js
@@ -0,0 +1,111 @@
+{
+  "basics": {
+    "name": "John Doe",
+    "label": "Programmer",
+    "picture": "https://randomuser.me/api/portraits/lego/1.jpg",
+    "email": "john@gmail.com",
+    "phone": "(912) 555-4321",
+    "website": "http://johndoe.com",
+    "summary": "Extremely motivated to constantly develop my skills and grow    professionally. I am confident in my ability to come up with interesting ideas  for unforgettable marketing campaigns.",
+    "location": {
+      "address": "2712 Broadway St",
+      "postalCode": "CA 94115",
+      "city": "San Francisco",
+      "countryCode": "US",
+      "region": "California"
+    },
+    "profiles": [{
+      "network": "Twitter",
+      "username": "john",
+      "url": "http://twitter.com/john"
+    }]
+  },
+  "work": [{
+    "company": "Company A",
+    "position": "Software Developer",
+    "website": "http://company.com",
+    "startDate": "2013-01-01",
+    "endDate": "2014-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Started the company",
+      "Grew the company"
+    ]
+  },
+  {
+    "company": "Company B",
+    "position": "Full stack Developer",
+    "website": "http://company.com",
+    "startDate": "2013-01-01",
+    "endDate": "2014-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Started the company",
+      "Grew the company"
+    ]
+  }],
+  "volunteer": [{
+    "organization": "Organization",
+    "position": "Volunteer",
+    "website": "http://organization.com/",
+    "startDate": "2012-01-01",
+    "endDate": "2013-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Awarded 'Volunteer of the Month'"
+    ]
+  }],
+  "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL",
+      "JS1021 - Int Javascript"
+    ]
+  }],
+  "awards": [{
+    "title": "Some Award",
+    "date": "2014-11-01",
+    "awarder": "Company A",
+    "summary": "There is no spoon."
+  }],
+  "publications": [{
+    "name": "Publication",
+    "publisher": "Company",
+    "releaseDate": "2014-10-01",
+    "website": "http://publication.com",
+    "summary": "Description..."
+  }],
+  "skills": [{
+    "name": "Web Development",
+    "level": "Master",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "Javascript"
+    ]
+  }],
+  "languages": [{
+    "language": "English",
+    "fluency": "Native speaker"
+  }, {
+    "language": "Vietnamese",
+    "fluency": "Native speaker"
+  }],
+  "interests": [{
+    "name": "Wildlife",
+    "keywords": [
+      "Ferrets",
+      "Unicorns"
+    ]
+  }],
+  "references": [{
+    "name": "Jane Doe",
+    "email": "janedoe@gmail.com",
+    "phone": "905-123-4567"
+  }]
+}
--- server/index.js
+++ server/index.js
@@ -2,11 +2,21 @@ const PORT          = 3001;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const fs            = require('fs');
 
-app.get('/', function (req, res) {
-  res.json('Hello World')
+let obj = ""
+
+fs.readFile('./data-files/resumeData.js', 'utf8', function (err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data)
+});
+
+
+app.get('/api/resume-data', function (req, res) {
+  
+  res.json(obj)
 })
  
 app.listen(PORT, () => {
-  console.log(Server is running on port: `${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
